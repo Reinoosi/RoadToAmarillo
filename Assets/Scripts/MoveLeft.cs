4b53939828d2db14dec52420830967eeffd5e98a
Reinoosi <brehe002@edu.xamk.fi>
2019-11-22 14:42:27 +0200

    Eka push

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 10f;
+    private PlayerController playerControllerScript;
+    private float leftBound = -15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move objects to the left until gameOver is true
+        if (playerControllerScript.gameOver == false)
+        {
+            // Move objects to left
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        // Destroy objects with Obstacle-tag when they reach -15 x.
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }   
+}
---
+++ Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float repeatWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        //Haetaan Backgroundista Boxcollider-komponentti ja sen koko ja jaetaan se x-akselin suunnassa kahdella.
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < startPos.x - repeatWidth)
+        {
+            transform.position = startPos;
+        }
+    }
+}
